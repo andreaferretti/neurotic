0bb4cc8f9378dc547279d024f8f9d87fe8cf3381
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-01-20 16:21:34 +0100

    Added loss

--- neurotic.nim
+++ neurotic.nim
@@ -1,24 +1,27 @@
-import future
+import future, math
 import linalg
 import random/urandom, random/mersenne
 
 type
   Module64 = concept m
-    var x: DMatrix64
+    # var x: DMatrix64
     var y: DVector64
     # m.forward(x) is DMatrix64
     m.forward(y) is DVector64
     # m.backward(x) is DMatrix64
     m.backward(y) is DVector64
   Cost64 = concept m
-    var x: DMatrix64
-    var y: DVector64
+    # var x: DMatrix64
+    var y, t: DVector64
     # m.forward(x) is DMatrix64
-    m.forward(y) is float64
+    m.forward(y, t) is float64
     # m.backward(x) is DMatrix64
-    m.backward(y) is float64
+    m.backward(y, t) is DVector64
   Layer64 = concept x
     x.withMemory is Module64
+  Result64 = object
+    loss: float64
+    gradient: DVector64
   Dense64 = object
     a, b: int
   Dense64Memory = object
@@ -30,6 +33,7 @@ type
   Sequential[A, B] = object
     module1: A
     module2: B
+  QuadraticCost = object
 
 var rng = initMersenneTwister(urandom(16))
 
@@ -70,19 +74,39 @@ proc backward[A, B: Module64](m: var Sequential[A, B], x: DVector64, eta = 0.01'
 proc `->`[A, B: Module64](a: A, b: B): auto =
   Sequential[A, B](module1: a, module2: b)
 
+proc forward(m: QuadraticCost, x, y: DVector64): float64 = l_2(x - y)
+
+proc backward(m: QuadraticCost, x, y: DVector64): DVector64 = 2 * (x - y)
+
+proc sigmoid(z: float64): float64 = 1.0 / (exp(-z) + 1.0)
+
+proc run(m: var Module64, c: Cost64, input, output: DVector64): Result64 =
+  let
+    prediction = m.forward(input)
+    loss = c.forward(prediction, output)
+    firstGradient = c.backward(prediction, output)
+    gradient = m.backward(firstGradient)
+  return Result64(loss: loss, gradient: gradient)
+
+# proc sigmoid_prime(z: Matrix): Matrix =
+#   sigmoid(z) .* (1.0 - sigmoid(z))
+
 when isMainModule:
   let
     l1 = dense(784, 30)
     l2 = dense(30, 20)
+    cost = QuadraticCost()
   var
     m1 = l1.withMemory
     m2 = l2.withMemory
     m3 = m1 -> m2
-  let v = randomVector(784).toDynamic
-  let output = m3.forward(v)
-  echo output
-  let x = m3.backward(output)
-  echo x
+  let
+    v = randomVector(784).toDynamic
+    w = randomVector(20).toDynamic
+  let result = run(m3, cost, v, w)
+  echo result.gradient
+  echo result.loss
   echo m1 is Module64
   echo l1 is Layer64
   echo m3 is Module64
+  echo cost is Cost64
