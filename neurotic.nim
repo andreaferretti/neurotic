4c8fb13dfcbc15b24f0214abf8233a2223c86d6f
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-01-20 18:50:01 +0100

    Iface

--- neurotic.nim
+++ neurotic.nim
@@ -1,15 +1,16 @@
-import future, math
+import future, math, sequtils
 import linalg
 import random/urandom, random/mersenne
+import collections/iface
 
 type
-  Module64 = concept m
-    # var x: DMatrix64
-    var y: DVector64
-    # m.forward(x) is DMatrix64
-    m.forward(y) is DVector64
-    # m.backward(x) is DMatrix64
-    # m.backward(y) is DVector64
+  # Module64 = concept m
+  #   # var x: DMatrix64
+  #   var y: DVector64
+  #   # m.forward(x) is DMatrix64
+  #   m.forward(y) is DVector64
+  #   # m.backward(x) is DMatrix64
+  #   m.backward(y) is DVector64
   Cost64 = concept m
     # var x: DMatrix64
     var y, t: DVector64
@@ -19,6 +20,7 @@ type
     m.backward(y, t) is DVector64
   Layer64 = concept x
     x.withMemory is Module64
+  IModule64 = distinct Interface
   Result64 = object
     loss: float64
     gradient: DVector64
@@ -27,18 +29,18 @@ type
   Dense64Memory = object
     weights: DMatrix64
     bias: DVector64
-  Dense64Module = object
+  Dense64Module = ref object of RootObj
     memory: Dense64Memory
     lastInput: DVector64
-  Sigmoid64 = object
+  Sigmoid64 = ref object of RootObj
     lastInput: DVector64
-  Sequential[A, B] = object
-    module1: A
-    module2: B
-  Closure64 = object
-    fwd: DVector64 -> DVector64
-    bkd: (DVector64, float64) -> DVector64
+  Sequential1 = ref object of RootObj
+    modules: seq[IModule64]
   QuadraticCost = object
+
+interfaceMethods IModule64:
+  forward(v: DVector64): DVector64
+  backward(v: DVector64, eta: float64): DVector64
 
 var rng = initMersenneTwister(urandom(16))
 
@@ -57,7 +59,7 @@ proc withMemory(d: Dense64, m: Dense64Memory): Dense64Module =
 
 proc withMemory(d: Dense64): Dense64Module = d.withMemory(d.memory)
 
-proc forward(m: var Dense64Module, x: DVector64): DVector64 =
+proc forward(m: Dense64Module, x: DVector64): DVector64 =
   m.lastInput = x
   return (m.memory.weights * x) + m.memory.bias
 
@@ -72,36 +74,22 @@ proc `|*|`(a, b: DVector64): DVector64 =
   for i in 0 .. < a.len:
     result[i] = a[i] * b[i]
 
-proc backward(m: var Dense64Module, v: DVector64, eta = 0.01'f64): DVector64 =
+proc backward(m: Dense64Module, v: DVector64, eta: float64): DVector64 =
   result = m.memory.weights.t * v
   let gradWeights = v .* m.lastInput
   m.memory.bias -= eta * v
   m.memory.weights -= eta * gradWeights
 
-proc forward[A, B: Module64](m: var Sequential[A, B], x: DVector64): DVector64 =
-  m.module2.forward(m.module1.forward(x))
+proc forward(m: Sequential1, x: DVector64): DVector64 =
+  m.modules.foldl(b.forward(a), x)
 
-proc backward[A, B: Module64](m: var Sequential[A, B], x: DVector64, eta = 0.01'f64): DVector64 =
-  m.module1.backward(m.module2.backward(x, eta), eta)
+proc backward(m: Sequential1, x: DVector64, eta: float64): DVector64 =
+  result = x
+  for i in countdown(m.modules.high, m.modules.low):
+    result = m.modules[i].backward(result, eta)
 
-proc forward(m: var Closure64, x: DVector64): DVector64 = m.fwd(x)
-
-proc backward(m: var Closure64, x: DVector64, eta = 0.01'f64): DVector64 = m.bkd(x, eta)
-
-proc into[A: Module64; B: Module64](a: A, b: B): Closure64 =
-  var a1 = a
-  var b1 = b
-
-  proc fwd(x: DVector64): DVector64 = b1.forward(a1.forward(x))
-
-  proc bkd(x: DVector64, eta: float64): DVector64 =
-    a1.backward(b1.backward(x, eta), eta)
-
-  return Closure64(fwd: fwd, bkd: bkd)
-
-
-proc `->`[A: Module64; B: Module64](a: var A, b: var B): auto =
-  Sequential[A, B](module1: a, module2: b)
+proc `->`[A; B](a: A, b: B): Sequential1 =
+  Sequential1(modules: @[a.asIModule64, b.asIModule64])
 
 proc forward(m: QuadraticCost, x, y: DVector64): float64 = l_2(x - y)
 
@@ -114,19 +102,19 @@ proc sigmoidPrime(z: float64): float64 = sigmoid(z) * (1.0 - sigmoid(z))
 makeUniversal(sigmoid)
 makeUniversal(sigmoidPrime)
 
-proc forward(m: var Sigmoid64, x: DVector64): DVector64 =
+proc forward(m: Sigmoid64, x: DVector64): DVector64 =
   m.lastInput = x
   return sigmoid(x)
 
-proc backward(m: var Sigmoid64, v: DVector64): DVector64 =
+proc backward(m: Sigmoid64, v: DVector64, eta: float64): DVector64 =
   sigmoidPrime(m.lastInput) |*| v
 
-proc run(m: var Module64, c: Cost64, input, output: DVector64): Result64 =
+proc run(m: IModule64, c: Cost64, input, output: DVector64, eta = 0.01'f64): Result64 =
   let
     prediction = m.forward(input)
     loss = c.forward(prediction, output)
     firstGradient = c.backward(prediction, output)
-    gradient = m.backward(firstGradient)
+    gradient = m.backward(firstGradient, eta)
   return Result64(loss: loss, gradient: gradient)
 
 
@@ -139,20 +127,17 @@ when isMainModule:
     m1 = l1.withMemory
     m2 = Sigmoid64()
     m3 = l2.withMemory
-    m4 = m1 -> m3
+    # m4 = m1 -> m3
+    m5 = m1 -> m2 -> m3
     # m5 = m3 -> m4
     # m5 = m4 -> m3
   let
     v = randomVector(784).toDynamic
     w = randomVector(20).toDynamic
-  let result = run(m4, cost, v, w)
+
+  let result = run(m5.asIModule64, cost, v, w)
   echo result.gradient
   echo result.loss
-  echo m1 is Module64
-  echo m2 is Module64
   # echo((m1 -> m2) is Module64)
   # echo((m2 -> m3) is Module64)
   # echo((m1 -> m3) is Module64)
-  echo l1 is Layer64
-  echo m2 is Module64
-  echo cost is Cost64
--- neurotic.nimble
+++ neurotic.nimble
@@ -7,7 +7,7 @@ license       = "Apache2"
 
 # Dependencies
 
-requires "nim >= 0.16.0", "random >= 0.5.3", "linalg >= 0.5.3"
+requires "nim >= 0.16.0", "random >= 0.5.3", "linalg >= 0.5.3", "collections >= 0.3.0"
 
 task run, "run example":
   --define:openblas
