4bab83a2658a02492431c9b59c1d3c7ff891fa1b
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-01-22 16:51:50 +0100

    More uniform style

--- neurotic/mnist.nim
+++ neurotic/mnist.nim
@@ -7,8 +7,8 @@ proc readInt32BE(s: FileStream): int =
   bigEndian32(addr tmp, addr r)
   result = tmp
 
-proc load_img_file(img_file: string, max_entries=high(int)): seq[DMatrix64] =
-  var s = newFileStream(img_file, fmRead)
+proc loadImgFile(imgFile: string, maxEntries = high(int)): seq[DMatrix64] =
+  var s = newFileStream(imgFile, fmRead)
   let magic = s.readInt32BE()
   assert(magic == 2051)
   let numImages = s.readInt32BE()
@@ -16,7 +16,7 @@ proc load_img_file(img_file: string, max_entries=high(int)): seq[DMatrix64] =
   let columns = s.readInt32BE()
 
   result = @[]
-  for n in 0..<min(numImages, max_entries):
+  for n in 0 ..< min(numImages, maxEntries):
     var img = zeros(rows, columns)
     for i in 0..<rows:
       for j in 0..<columns:
@@ -24,24 +24,24 @@ proc load_img_file(img_file: string, max_entries=high(int)): seq[DMatrix64] =
     result.add(img)
   s.close()
 
-proc load_label_file(label_file: string, max_entries=high(int)): seq[int] =
-  var s = newFileStream(label_file, fmRead)
+proc loadLabelFile(labelFile: string, maxEntries = high(int)): seq[int] =
+  var s = newFileStream(labelFile, fmRead)
   let magic = s.readInt32BE()
   assert(magic == 2049)
   let numItems = s.readInt32BE()
 
   result = @[]
-  for n in 0..<min(numItems, max_entries):
+  for n in 0 ..< min(numItems, maxEntries):
     result.add(cast[int](s.readChar()))
   s.close()
 
-proc mnist_load*(img_file, label_file: string, max_entries=high(int)): auto =
+proc mnist_load*(imgFile, labelFile: string, maxEntries = high(int)): auto =
   proc vectorize(i: int): DVector64 =
     let m = 10
     result = zeros(m)
     result[i] = 1.0
 
-  let images = load_img_file(img_file, max_entries=max_entries)
-  let labels = load_label_file(label_file, max_entries=max_entries)
+  let images = loadImgFile(imgFile, maxEntries = maxEntries)
+  let labels = loadLabelFile(labelFile, maxEntries = maxEntries)
   assert(len(images) == len(labels))
   result = zip(images, labels.map(vectorize))
