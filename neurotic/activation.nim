827bb34d35b5bfbca4cbcb89ab98801c9f1d5e95
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-02-17 11:04:56 +0100

    Started adding 32-bit support

--- neurotic/activation.nim
+++ neurotic/activation.nim
@@ -2,6 +2,13 @@ import linalg
 import ./core, ./util
 
 type
+  Activation32* = ref object of Layer32
+    lastInput: DVector32
+    lastInputs: DMatrix32
+    f: proc(x: DVector32): DVector32
+    fm: proc(x: DMatrix32): DMatrix32
+    fPrime: proc(x: DVector32): DVector32
+    fmPrime: proc(x: DMatrix32): DMatrix32
   Activation64* = ref object of Layer64
     lastInput: DVector64
     lastInputs: DMatrix64
@@ -10,19 +17,33 @@ type
     fPrime: proc(x: DVector64): DVector64
     fmPrime: proc(x: DMatrix64): DMatrix64
 
-proc sigmoid*(z: float64): float64 = 1.0 / (exp(-z) + 1.0)
+proc sigmoid*(z: float32 or float64): auto = 1.0 / (exp(-z) + 1.0)
 
-proc sigmoidPrime*(z: float64): float64 = sigmoid(z) * (1.0 - sigmoid(z))
+proc sigmoidPrime*(z: float32 or float64): auto = sigmoid(z) * (1.0 - sigmoid(z))
 
-proc relu*(z: float64): float64 = max(z, 0.0)
+proc relu*(z: float32 or float64): auto = max(z, 0.0)
 
-proc reluPrime*(z: float64): float64 =
+proc reluPrime*(z: float32 or float64): auto =
   if z >= 0: 1.0 else: 0.0
 
 makeUniversal(sigmoid)
 makeUniversal(sigmoidPrime)
 makeUniversal(relu)
 makeUniversal(reluPrime)
+
+method forward*(a: Activation32, x: DVector32): DVector32 =
+  a.lastInput = x
+  return a.f(x)
+
+method forward*(a: Activation32, x: DMatrix32): DMatrix32 =
+  a.lastInputs = x
+  return a.fm(x)
+
+method backward*(a: Activation32, v: DVector32, eta: float32): DVector32 =
+  a.fPrime(a.lastInput) |*| v
+
+method backward*(a: Activation32, v: DMatrix32, eta: float32): DMatrix32 =
+  a.fmPrime(a.lastInputs) |*| v
 
 method forward*(a: Activation64, x: DVector64): DVector64 =
   a.lastInput = x
@@ -43,6 +64,20 @@ proc sigmoidModule*(): Activation64 = Activation64(
   fm: sigmoid,
   fPrime: sigmoidPrime,
   fmPrime: sigmoidPrime
+)
+
+proc sigmoidModule32*(): Activation32 = Activation32(
+  f: sigmoid,
+  fm: sigmoid,
+  fPrime: sigmoidPrime,
+  fmPrime: sigmoidPrime
+)
+
+proc reluModule32*(): Activation32 = Activation32(
+  f: relu,
+  fm: relu,
+  fPrime: reluPrime,
+  fmPrime: reluPrime
 )
 
 proc reluModule*(): Activation64 = Activation64(
--- neurotic/core.nim
+++ neurotic/core.nim
@@ -1,6 +1,13 @@
 import linalg
 
 type
+  Cost32* = concept c
+    var x, s: DMatrix32
+    var y, t: DVector32
+    c.forward(x, s) is float32
+    c.forward(y, t) is float32
+    c.backward(x, s) is DMatrix32
+    c.backward(y, t) is DVector32
   Cost64* = concept c
     var x, s: DMatrix64
     var y, t: DVector64
@@ -8,7 +15,20 @@ type
     c.forward(y, t) is float64
     c.backward(x, s) is DMatrix64
     c.backward(y, t) is DVector64
+  Layer32* = ref object of RootObj
   Layer64* = ref object of RootObj
+
+method forward*(m: Layer32, v: DVector32): DVector32 {.base.} =
+  quit "to override!"
+
+method forward*(m: Layer32, v: DMatrix32): DMatrix32 {.base.} =
+  quit "to override!"
+
+method backward*(m: Layer32, v: DVector32, eta: float32): DVector32 {.base.} =
+  quit "to override!"
+
+method backward*(m: Layer32, v: DMatrix32, eta: float32): DMatrix32 {.base.} =
+  quit "to override!"
 
 method forward*(m: Layer64, v: DVector64): DVector64 {.base.} =
   quit "to override!"
